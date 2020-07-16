428cb4bfb70f31c7d349292bb36e11fd4da8cb5c
github username <github email address>
2020-07-16 11:30:59 -0700

    add login logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Dashboard from './containers/Dashboard'
+import React, { Component } from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import Dashboard from "./containers/Dashboard";
+import Login from "./components/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <Dashboard />
-    </div>
-  );
+class App extends Component {
+  state = {
+    auth: {
+      user: {},
+    },
+  };
+
+  // on submit from Login.js
+  onLogin = (token) => {
+    console.log(`loggin in ... ${token.username}`)
+    // const updatedState = {
+    //   ...this.state.auth,
+    //   user: { id: token.id, name: token.name },
+    // };
+    // localStorage.setItem("token", token.jwt);
+    // this.setState({ auth: updatedState });
+  };
+
+  // // on submit from pending Logout button
+  // onLogout = () => {
+  //   localStorage.removeItem("token");
+  //   this.setState({
+  //     auth: {
+  //       user: {},
+  //     },
+  //   });
+  // };
+
+  render() {
+    return (
+      <div className="App">
+        <Login onLogin={this.onLogin} />
+        <Dashboard />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { api } from "../services/api";
+import { Link } from "react-router-dom";
+
+class Login extends Component {
+  state = {
+    error: false,
+    fields: {
+      username: "",
+      password: "",
+    },
+  };
+
+  handleChange = (e) => {
+    const newFields = { ...this.state.fields, [e.target.name]: e.target.value };
+    this.setState({
+      fields: newFields,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onLogin(this.state.fields);
+
+    // // fetch request to login from /services/api.js
+    // api.auth.login(this.state.fields).then((resp) => {
+    //   if (!resp.error) {
+    //     this.props.onLogin(resp);
+    //     this.props.history.push("/dashboard");
+    //   } else {
+    //     this.setState({ eror: true });
+    //   }
+    // });
+  };
+
+  render() {
+    const { fields } = this.state;
+    return (
+      <div>
+        Hi from Login
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <label>Username</label>
+              <input
+                name="username"
+                placeholder="Enter username"
+                value={fields.username}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <label>Password</label>
+              <input
+                name="password"
+                type="password"
+                placeholder="Enter password"
+                value={fields.password}
+                onChange={this.handleChange}
+              />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
