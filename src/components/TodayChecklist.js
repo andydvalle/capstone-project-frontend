2f6f6d3087811474e95f689d143899fb2b280c41
github username <github email address>
2020-07-21 10:49:18 -0700

    add handleChange console log

--- src/components/TodayChecklist.js
+++ src/components/TodayChecklist.js
@@ -1,17 +1,24 @@
-import React from "react";
-import Today from "../containers/Today";
+import React, {useState} from "react";
+// import * as lib from 'strikethrough-js';
 
 const TodayChecklist = (props) => {
+
+    const [med, setMed] = useState(props.medication.name_route)
+
+    const handleChange = (e) =>{
+        console.log(`${e.target.value} is ${e.target.checked}`)
+    }
+
   return (
-    <div class="form-check">
+    <div class="form-check" onChange={handleChange}>
       <input
         className="form-check-input"
         type="checkbox"
-        value=""
+        value={props.medication.name_route}
         id="defaultCheck1"
       />
       <label className="form-check-label" htmlFor="defaultCheck1">
-        {props.medication.name_route}
+        {med}
       </label>
     </div>
   );
