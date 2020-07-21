0f40a33a3266d248dd47ba9d912ed518688ed092
github username <github email address>
2020-07-21 10:19:56 -0700

    add renderMeds logic to Today

--- src/containers/Today.js
+++ src/containers/Today.js
@@ -4,13 +4,13 @@ const Today = (props) => {
   const weekday = () => {
     var d = new Date();
     var weekday = new Array(7);
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
+    weekday[0] = "onSun";
+    weekday[1] = "onMon";
+    weekday[2] = "onTue";
+    weekday[3] = "onWed";
+    weekday[4] = "onThu";
+    weekday[5] = "onFri";
+    weekday[6] = "onSat";
 
     var n = weekday[d.getDay()];
     return n;
@@ -18,16 +18,26 @@ const Today = (props) => {
 
   const [day, setDay] = useState(weekday());
 
-  const renderMeds = () =>{
-      props.foundProfile.medications.map(medication=>console.log(medication.onMon))
-  }
+  const renderMeds = () => {
+    props.foundProfile.medications.map((medication) => {
+      Object.keys(medication).map((key) => {
+        if (key === day) {
+          if (key) {
+            console.log(key);
+          } else {
+            return null;
+          }
+        } else {
+          return null;
+        }
+      });
+    });
+  };
 
   return (
     <div className="main_content">
       Today's Schedule {props.foundProfile.firstName}
-      <div>
-          Medications
-      </div>
+      <div>Medications</div>
       {renderMeds()}
     </div>
   );
