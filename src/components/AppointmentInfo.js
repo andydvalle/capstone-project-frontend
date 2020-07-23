ff3e25c4d4b574fb864881c926525652c71e989d
github username <github email address>
2020-07-23 12:07:46 -0700

    clean appointmentInfo

--- src/components/AppointmentInfo.js
+++ src/components/AppointmentInfo.js
@@ -32,34 +32,15 @@ const AppointmentInfo = (props) => {
 
   return (
     <div ref={wrapperRef} className="dropdown-info">
-      {isEdit ? null : props.appointment.title}
       {isEdit ? (
         <AppointmentForm
           isEdit={isEdit}
           appointment={props.appointment}
           clinics={props.clinics}
         />
-      ) : null}
-      {/* <form>
-        <div className="form-row">
-          <div className="form-group col-md-6">
-            <label>Title:</label>
-            <input type="text" className="form-control" {...title} />
-          </div>
-          <div className="form-group col-md-3">
-            <label>Date:</label>
-            <input type="text" className="form-control" {...date} />
-          </div>
-          <div className="form-group col-md-3">
-            <label>Time:</label>
-            <input type="text" className="form-control" {...time} />
-          </div>
-        </div>
-        <div className="form-group">
-          <label>Notes:</label>
-          <input type="text" className="form-control" {...notes} readonly />
-        </div>
-      </form> */}
+      ) : (
+        props.appointment.title
+      )}
       <div>
         <span className="mr-3" onClick={handleEdit}>
           Edit
