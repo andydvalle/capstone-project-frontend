661c17b29c3144727a40665e362fdb6338b78ce7
github username <github email address>
2020-07-23 11:44:31 -0700

    add isEdit toggle

--- src/components/AppointmentInfo.js
+++ src/components/AppointmentInfo.js
@@ -1,9 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import useFormInput from "../FormInput";
+import AppointmentForm from "./AppointmentForm";
 
 const AppointmentInfo = (props) => {
-  // const []
+  const [isEdit, setIsEdit] = useState(false);
+  const wrapperRef = useRef(null);
 
-  return <div className="dropdown-info">{props.appointment.title}</div>;
+  const title = useFormInput(props.appointment.title);
+  const date = useFormInput(props.appointment.date);
+  const time = useFormInput(props.appointment.time);
+  const notes = useFormInput(props.appointment.notes);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = (e) => {
+    const { current: wrap } = wrapperRef;
+    if (wrap && !wrap.contains(e.target)) {
+      setIsEdit(false);
+    }
+  };
+
+  const handleEdit = () => {
+    setIsEdit(true);
+  };
+
+  return (
+    <div ref={wrapperRef} className="dropdown-info">
+      {props.appointment.title}
+      {isEdit ? <AppointmentForm /> : null}
+      {/* <form>
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <label>Title:</label>
+            <input type="text" className="form-control" {...title} />
+          </div>
+          <div className="form-group col-md-3">
+            <label>Date:</label>
+            <input type="text" className="form-control" {...date} />
+          </div>
+          <div className="form-group col-md-3">
+            <label>Time:</label>
+            <input type="text" className="form-control" {...time} />
+          </div>
+        </div>
+        <div className="form-group">
+          <label>Notes:</label>
+          <input type="text" className="form-control" {...notes} readonly />
+        </div>
+      </form> */}
+      <div>
+        <span className="mr-3" onClick={handleEdit}>
+          Edit
+        </span>
+        <span>Delete</span>
+      </div>
+    </div>
+  );
 };
 
 export default AppointmentInfo;
