b63df46e9308aedd79dfd1fb139403a48df44b33
github username <github email address>
2020-07-26 13:29:19 -0700

    add ellipse login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { api } from "../services/api";
+import Ellipse2 from "../svgs/Ellipse2";
+import Ellipse3 from "../svgs/Ellipse3";
+import Ellipse4 from "../svgs/Ellipse4";
+import Ellipse5 from "../svgs/Ellipse5";
 
 class Login extends Component {
   state = {
@@ -34,39 +38,61 @@ class Login extends Component {
   render() {
     const { fields } = this.state;
     return (
-      <div className="login">
-        <div className="login-header">
-          Your health is important. Let's keep you on track.
-        </div>
-        <div className="login-form">
-          {this.state.error ? <p>Please try again</p> : null}
-          <form onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <label className="login-label">Username</label>
-              <input
-                className="form-control"
-                type="text"
-                name="username"
-                placeholder="Your username"
-                value={fields.username}
-                onChange={this.handleChange}
-              />
-              {/* <small id="emailHelp" className="form-text text-muted">
+      <div>
+        <div className="login-overlay-wrap">
+          <div className="login-ellipses">
+            <div className="row">
+              <div id="ellipse2">
+                <Ellipse2 />
+              </div>
+              <div id="ellipse3">
+                <Ellipse3 />
+              </div>
+            </div>
+            <div className="row">
+              <div id="ellipse4">
+                <Ellipse4 />
+              </div>
+              <div id="ellipse5">
+                <Ellipse5 />
+              </div>
+            </div>
+          </div>
+          <div className="notelist">
+            <div className="login">
+              <div className="login-header">
+                Your health is important. Let's keep you on track.
+              </div>
+              <div className="login-form">
+                {this.state.error ? <p>Please try again</p> : null}
+                {/* ellipses */}
+                <form onSubmit={this.handleSubmit}>
+                  <div className="form-group">
+                    <label className="login-label">Username</label>
+                    <input
+                      className="form-control"
+                      type="text"
+                      name="username"
+                      placeholder="Your username"
+                      value={fields.username}
+                      onChange={this.handleChange}
+                    />
+                    {/* <small id="emailHelp" className="form-text text-muted">
               We'll never share your email with anyone else.
             </small> */}
-            </div>
-            <div className="form-group">
-              <label className="login-label">Password</label>
-              <input
-                className="form-control"
-                name="password"
-                type="password"
-                placeholder="Your password"
-                value={fields.password}
-                onChange={this.handleChange}
-              />
-            </div>
-            {/* <div className="form-check">
+                  </div>
+                  <div className="form-group">
+                    <label className="login-label">Password</label>
+                    <input
+                      className="form-control"
+                      name="password"
+                      type="password"
+                      placeholder="Your password"
+                      value={fields.password}
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                  {/* <div className="form-check">
             <input
             type="checkbox"
             className="form-check-input"
@@ -76,15 +102,18 @@ class Login extends Component {
             Check me out
             </label>
           </div> */}
-            <div className="form-row">
-              <button class="btn btn-primary btn mt-3" type="submit">
-                Login
-              </button>
-              <div className="login-signup">
-                <Link to={`/signup`}>Not a member? Signup!</Link>
+                  <div className="form-row">
+                    <button class="btn btn-primary btn mt-3" type="submit">
+                      Login
+                    </button>
+                    <div className="login-signup">
+                      <Link to={`/signup`}>Not a member? Signup!</Link>
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     );
