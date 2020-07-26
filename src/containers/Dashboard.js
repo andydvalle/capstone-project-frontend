fe67be7f63d1dbb047b7a0ab1d94fd8ac48cd25d
github username <github email address>
2020-07-26 10:56:13 -0700

    cadd dashboard-form css

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -65,42 +65,43 @@ class Dashboard extends Component {
               </div>
               <div>
                 <h1>Select a profile to view:</h1>
-                {this.renderProfiles()}
-              </div>
-            </div>
-            {/* form modal */}
-            <div>
-              {/* <Link to="/login">
+                <div className="row">{this.renderProfiles()}</div>
+
+                {/* form modal */}
+                <div className="dashboard-modal">
+                  {/* <Link to="/login">
               <button onClick={this.handleClick}>Signout</button>
             </Link> */}
 
-              <button
-                type="button"
-                id="add-form"
-                className="btn btn-primary"
-                data-toggle="modal"
-                data-target=".modal"
-              >
-                +
-              </button>
-            </div>
-            {/* form modal content */}
-            <div className="modal" tabindex="-1" role="dialog">
-              <div className="modal-dialog" role="document">
-                <div className="modal-content">
-                  <div className="form-modal">
-                    New Profile
-                    <button
-                      type="button"
-                      className="close"
-                      data-dismiss="modal"
-                      aria-label="Close"
-                    >
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div className="modal-body">
-                    <ProfileForm currentUser={this.props.user} />
+                  <button
+                    type="button"
+                    id="dashboard-form"
+                    className="btn btn-primary"
+                    data-toggle="modal"
+                    data-target=".modal"
+                  >
+                    +
+                  </button>
+                </div>
+                {/* form modal content */}
+                <div className="modal" tabindex="-1" role="dialog">
+                  <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                      <div className="form-modal">
+                        New Profile
+                        <button
+                          type="button"
+                          className="close"
+                          data-dismiss="modal"
+                          aria-label="Close"
+                        >
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div className="modal-body">
+                        <ProfileForm currentUser={this.props.user} />
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
