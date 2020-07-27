fbe5b7dddfd442a4b9cf0dd8ba993f1bb3fbcc42
github username <github email address>
2020-07-26 17:07:28 -0700

    add ellipses too singup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { api } from "../services/api";
 import { Link } from "react-router-dom";
+import Ellipse2 from "../svgs/Ellipse2";
+import Ellipse3 from "../svgs/Ellipse3";
+import Ellipse4 from "../svgs/Ellipse4";
+import Ellipse5 from "../svgs/Ellipse5";
 
 const Signup = (props) => {
   const [username, setUsername] = useState("");
@@ -42,56 +46,80 @@ const Signup = (props) => {
   };
 
   return (
-    <div className="login">
-      <div className="login-header">
-        Ready to get started? Let's sign you up!
-      </div>{" "}
-      {error ? <h5>Username already taken, please try again.</h5> : null}
-      <form onSubmit={handleSubmit}>
-        <div className="login-form">
-          <div className="form-group">
-            <label className="login-label">Username</label>
-            <input
-              name="username"
-              class="form-control"
-              placeholder="Enter username"
-              value={username}
-              onChange={handleChange}
-            />
+    <div>
+      <div className="login-overlay-wrap">
+        <div className="login-ellipses">
+          <div className="row">
+            <div id="ellipse2">
+              <Ellipse2 />
+            </div>
+            <div id="ellipse3">
+              <Ellipse3 />
+            </div>
           </div>
-          <div className="form-group">
-            <label className="login-label">Password</label>
-            {unmatch ? <p>Passwords do not match, try again.</p> : null}
-            <input
-              name="password"
-              type="password"
-              class="form-control"
-              placeholder="Password"
-              value={password}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-group">
-            <label className="login-label">Confirm Password</label>
-            <input
-              name="confirm"
-              type="password"
-              class="form-control"
-              placeholder="Password"
-              value={confirm}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-row">
-            <button class="btn btn-primary btn mt-3" type="submit">
-              Sign up
-            </button>
-            <div className="login-signup">
-              <Link to={`/`}>Have an account? Login!</Link>
+          <div className="row">
+            <div id="ellipse4">
+              <Ellipse4 />
+            </div>
+            <div id="ellipse5">
+              <Ellipse5 />
             </div>
           </div>
         </div>
-      </form>
+        <div className="notelist">
+          <div className="login">
+            <div className="login-header">
+              Ready to get started? Let's sign you up!
+            </div>{" "}
+            {error ? <h5>Username already taken, please try again.</h5> : null}
+            <form onSubmit={handleSubmit}>
+              <div className="login-form">
+                <div className="form-group">
+                  <label className="login-label">Username</label>
+                  <input
+                    name="username"
+                    class="form-control"
+                    placeholder="Enter username"
+                    value={username}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label className="login-label">Password</label>
+                  {unmatch ? <p>Passwords do not match, try again.</p> : null}
+                  <input
+                    name="password"
+                    type="password"
+                    class="form-control"
+                    placeholder="Password"
+                    value={password}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label className="login-label">Confirm Password</label>
+                  <input
+                    name="confirm"
+                    type="password"
+                    class="form-control"
+                    placeholder="Password"
+                    value={confirm}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="form-row">
+                  <button class="btn btn-primary btn mt-3" type="submit">
+                    Sign up
+                  </button>
+                  <div className="login-signup">
+                    <Link to={`/`}>Have an account? Login!</Link>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
