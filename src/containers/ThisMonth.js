8af756db5bdf46dfdd4bdb7f38d27cf67baf9337
github username <github email address>
2020-07-27 10:53:21 -0700

    add useState to calendar

--- src/containers/ThisMonth.js
+++ src/containers/ThisMonth.js
@@ -1,5 +1,5 @@
-import React from "react";
-import events from "../events";
+import React, { useState } from "react";
+import exampleEvents from "../events";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
@@ -7,13 +7,14 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 const localizer = momentLocalizer(moment);
 
 const ThisMonth = (props) => {
+  const [events, setEvents] = useState([]);
   return (
     <div className="main_content">
       <div className="header">{props.foundProfile.firstName}'s Calendar</div>
       <div>
         <Calendar
           localizer={localizer}
-          events={events}
+          events={exampleEvents}
           startAccessor="start"
           endAccessor="end"
           style={{ height: 500 }}
