7723d8af3975b56dbbbee82e9fb2c53b3c7149e5
github username <github email address>
2020-07-27 10:50:59 -0700

    removed extra calendar component

--- src/containers/ThisMonth.js
+++ src/containers/ThisMonth.js
@@ -1,12 +1,24 @@
 import React from "react";
-import Calendar from "../components/Calendar";
+import events from "../events";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
 
 const ThisMonth = (props) => {
   return (
     <div className="main_content">
-      <div className="header">Month Schedule</div>
-      Hi from thisMonth
-      <Calendar />
+      <div className="header">{props.foundProfile.firstName}'s Calendar</div>
+      <div>
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+        />
+      </div>
     </div>
   );
 };
