de0f71e26bc24d2f78bd1d8248adae54684f0bc0
github username <andydelvalle89@gmail.com>
2020-08-15 10:59:00 -0700

    remove tooltip

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -13,26 +13,24 @@ const Profile = (props) => {
   };
 
   return (
-    <div data-toggle="tooltip" data-placement="bottom" title="Hooray!">
-      <div className="card m-2" style={{ width: "10rem" }}>
-        <div className="card-body">
-          <Link to={`/dashboard/${id}/view`}>
-            <h5 className="card-title">{firstName}</h5>{" "}
-          </Link>
-          <h6 className="card-subtitle mb-2 text-muted">{lastName}</h6>
-          <a
-            href="#"
-            className="card-link"
-            data-toggle="modal"
-            data-target=".modal"
-            onClick={handleEdit}
-          >
-            Edit
-          </a>
-          <a href="#" className="card-link" onClick={handleDelete}>
-            Delete
-          </a>
-        </div>
+    <div className="card m-2" style={{ width: "10rem" }}>
+      <div className="card-body">
+        <Link to={`/dashboard/${id}/view`}>
+          <h5 className="card-title">{firstName}</h5>{" "}
+        </Link>
+        <h6 className="card-subtitle mb-2 text-muted">{lastName}</h6>
+        <a
+          href="#"
+          className="card-link"
+          data-toggle="modal"
+          data-target=".modal"
+          onClick={handleEdit}
+        >
+          Edit
+        </a>
+        <a href="#" className="card-link" onClick={handleDelete}>
+          Delete
+        </a>
       </div>
     </div>
   );
