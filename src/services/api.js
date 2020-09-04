aec4a64eb8e4cf2cec5e0902fd974d19fe2870dc
github username <andydelvalle89@gmail.com>
2020-09-04 12:13:12 -0700

    npm i

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,4 @@
 const API_ROOT = `https://simple-care-app-api.herokuapp.com/api/v1`;
-// const API_ROOT = `http://localhost:3000/api/v1`;
 
 //grabs the token held in browser's localStorage
 const token = () => localStorage.getItem("token");
